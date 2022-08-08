5dbbf03e1b917491b2e5e6dc1584434ae8e5ebe0
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-08-08 11:40:22 +0530

    pattern
    java

---
+++ patterns.java
@@ -0,0 +1,40 @@
+package visha_theanmozhi;
+
+public class patterns {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int size=5;
+//		for(int i=0;i<size;i++) {
+//			for(int j=0;j<size;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		//left triangle
+//		int size=5;
+//		for(int i=0;i<size;i++) {
+//			for(int j=0;j<=i;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		//left triangle
+		int size=5;
+		for (int i=0;i<size;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print(j+1);
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+      
+
+
+
+
